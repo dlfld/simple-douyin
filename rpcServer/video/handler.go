261040b64fd130b46a9f22cbbce3d735b2b033f5
戴林峰 <dailinfeng66@163.com>
2023-08-01 22:08:11 +0800

    fix: 视频查询接口中，数据库查询写法

--- rpcServer/video/handler.go
+++ rpcServer/video/handler.go
@@ -3,8 +3,8 @@ package main
 import (
 	"context"
 	"github.com/douyin/kitex_gen/video"
+	"github.com/douyin/models"
 	"github.com/douyin/rpcServer/video/convert"
-	"github.com/douyin/rpcServer/video/db"
 	"net/http"
 	"strconv"
 )
@@ -28,7 +28,7 @@ func (s *VideoServiceImpl) PublishAction(ctx context.Context, req *video.Publish
 // 获取登录用户的视频发布列表，直接列出用户所有投稿过的视频。
 func (s *VideoServiceImpl) PublishList(ctx context.Context, req *video.PublishListRequest) (resp *video.PublishListResponse, err error) {
 	// 根据登陆用户的id，查询用户所投稿过的所有视频
-	videoList, err := db.FindVideoListBy("id", strconv.Itoa(int(req.GetUserId())))
+	videoList, err := models.FindVideoListBy("id", strconv.Itoa(int(req.GetUserId())))
 	if err != nil {
 		return nil, err
 	}

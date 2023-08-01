af0b8d8f52777f21a9a12b7b949aaf2741b54b85
戴林峰 <dailinfeng66@163.com>
2023-08-01 22:03:27 +0800

    fix: OSS Service中的注释

--- common/oss/ossService.go
+++ common/oss/ossService.go
@@ -2,8 +2,8 @@ package oss
 
 import (
 	"github.com/douyin/common/oss/minioService"
-	"sync"
 	"io"
+	"sync"
 )
 
 // OssInterface Oss 服务接口
@@ -14,14 +14,10 @@ type OssInterface interface {
 	CreateBucket(bucketName string) error
 	// DeleteBucket 删除桶
 	DeleteBucket(bucketName string) error
-	// // 上传Video文件
-	// UploadVideo(bucketName string, filePath string) error
-	// 上传文件
+	// UploadFile  上传文件
 	UploadFile(bucketName string, filePath string, contentType string) error
-	// 上传文件(传入字节流)
+	// UploadFileWithBytestream 上传文件(传入字节流)
 	UploadFileWithBytestream(bucketName string, reader io.Reader, fileName string, fileSize int64, contentType string) error
-
-	
 }
 
 // Service OssService结构体
@@ -33,6 +29,11 @@ type Service struct {
 // 通过反射获取当前包下具体的实现软件，然后拉进来？
 //然后使用可配置的方式配置具体的对象存储软件？
 
+//	GetOssService
+//
+// @Description: 获取OSS服务端
+// @return *Service
+// @return error
 func GetOssService() (*Service, error) {
 	var once sync.Once
 	var service *Service
@@ -51,28 +52,47 @@ func GetOssService() (*Service, error) {
 	return service, nil
 }
 
+//	GetClient
+//
+// @Description: 获取客户端
+// @receiver service
+// @return interface{}
+// @return error
 func (service *Service) GetClient() (interface{}, error) {
 	//TODO 这里涉及到了调用具体的minio的方法，（后面看看能否改为反射的方式）
 	return service.ossService.(*minioService.MinioService).Client, nil
 }
 
+//	CreateBucket
+//
+// @Description: 根据桶名创建桶
+// @receiver service
+// @param bucketName
+// @return error
 func (service *Service) CreateBucket(bucketName string) error {
 	return service.ossService.(*minioService.MinioService).CreateBucket(bucketName)
 }
 
+//	DeleteBucket
+//
+// @Description: 根据桶名删除桶
+// @receiver service
+// @param bucketName
+// @return error
 func (service *Service) DeleteBucket(bucketName string) error {
 	return service.ossService.(*minioService.MinioService).DeleteBucket(bucketName)
 }
 
-// func (service *Service) UploadVideo(bucketName string, filePath string) error {
-// 	return service.ossService.(*minioService.MinioService).UploadVideo(bucketName, filePath)
-// }
-
-
+// UploadFile
+// @Description: 上传文件
+// @receiver service
+// @param bucketName 桶名字
+// @param filePath 文件位置
+// @param contentType  文件类型(image/jpeg video/mp4)
+// @return error
 func (service *Service) UploadFile(bucketName string, filePath string, contentType string) error {
 	return service.ossService.(*minioService.MinioService).UploadFile(bucketName, filePath, contentType)
 }
-
 
 // UploadFileWithBytestream
 // @Description: 通用的文件上传函数(传入字节流, 信息从用户上传的 HTTP 请求中获取)

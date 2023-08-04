16b8fa379e8cced28dd49a2c202ed8de8cd799a2
戴林峰 <dailinfeng66@163.com>
2023-08-04 21:20:24 +0800

    update:video todo

--- rpcServer/video/handler.go
+++ rpcServer/video/handler.go
@@ -28,7 +28,7 @@ func (s *VideoServiceImpl) PublishAction(ctx context.Context, req *video.Publish
 	reader := bytes.NewReader(req.GetData())
 	// 上传文件的文件名
 	filename := req.GetTitle()
-	// TODO 根据Token获取用户信息，然后根据用户信息写入用户投稿的视频
+	// TODO 根据Token获取用户信息，然后根据用户信息写入用户投稿的视频，在redis中加入这一条视频
 	//videoUrl := fmt.Sprintf("http://%s/%s/%s", conf.MinioConfig.IP, conf.MinioConfig.VideoBucketName, filename)
 
 	// TODO 魔法值需要改

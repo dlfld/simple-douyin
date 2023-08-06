2b4e25c4a64433a27138909d1f914c76b99eec76
wdk <2563347014@qq.com>
2023-08-06 10:13:53 +0800

    添加部分message功能

--- handler/message.go
+++ handler/message.go
@@ -1,11 +1,13 @@
 package handler
 
 import (
-	"context"
+	"fmt"
 	"github.com/douyin/kitex_gen/message"
+	"github.com/douyin/kitex_gen/model"
 	"github.com/douyin/rpcClient/messageRpc"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 func MessageAction(c *gin.Context) {
@@ -18,10 +20,45 @@ func MessageAction(c *gin.Context) {
 	req := message.NewMessageActionRequest()
 	// 3. 前端请求数据绑定到req中
 	_ = c.ShouldBind(req)
+	fmt.Println(req)
+	temUid, _ := strconv.Atoi(c.Query("userID"))
+	uid := int64(temUid)
+	user := model.BaseReq{
+		UserId: &uid,
+	}
+	req.User = &user
 	// 4. 发起RPC调用
-	resp, err := cli.SendMessage(context.Background(), req)
+	//ctx := context.WithValue(context.Background(), "userID", c.Query(""))
+	resp, err := cli.SendMessage(c, req)
+	if err != nil {
+		c.JSON(http.StatusOK, resp)
+	}
+	// 5. gin返回给前端
+	c.JSON(http.StatusOK, resp)
+}
+
+func MessageChat(c *gin.Context) {
+	// 1. 创建客户端连接
+	cli, err := messageRpc.NewRpcMessageClient()
 	if err != nil {
 		panic(err)
+	}
+	// 2. 创建请求实例
+	req := message.NewMessageChatRequest()
+	// 3. 前端请求数据绑定到req中
+	_ = c.ShouldBind(req)
+	fmt.Println(req)
+	temUid, _ := strconv.Atoi(c.Query("userID"))
+	uid := int64(temUid)
+	user := model.BaseReq{
+		UserId: &uid,
+	}
+	req.User = &user
+	// 4. 发起RPC调用
+	//ctx := context.WithValue(context.Background(), "userID", c.Query(""))
+	resp, err := cli.MessageList(c, req)
+	if err != nil {
+		c.JSON(http.StatusOK, resp)
 	}
 	// 5. gin返回给前端
 	c.JSON(http.StatusOK, resp)
--- rpcServer/message/init.go
+++ rpcServer/message/init.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"github.com/douyin/common/mysql"
+	rdb "github.com/douyin/common/redis"
+	"github.com/douyin/models"
+	"github.com/go-redis/redis/v8"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+var cache *redis.Client
+var err error
+
+func init() {
+	db, err = mysql.NewMysqlConn()
+	if err != nil {
+		panic(ErrMysqlConn)
+	}
+	if err := db.AutoMigrate(&models.Message{}); err != nil {
+		panic(err)
+	}
+	cache, err = rdb.NewRedisConn()
+	if err != nil {
+		panic(ErrCacheConn)
+	}
+}

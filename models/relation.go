599a25ab83659dc855e1a6a48aa5ca80bcf31b79
Jinxin Xiong <xjxkeep@gmail.com>
2023-08-30 12:20:10 +0800

    relation数据库模型更新

--- models/relation.go
+++ models/relation.go
@@ -30,7 +30,6 @@ func init() {
 //
 //	@Description: 用户之间的关注关系数据模型
 type FollowRelation struct {
-	gorm.Model
 	User     User `gorm:"foreignkey:UserID;" json:"user,omitempty"`
 	UserID   uint `gorm:"index:idx_userid;not null" json:"user_id"`
 	ToUser   User `gorm:"foreignkey:ToUserID;" json:"to_user,omitempty"`
